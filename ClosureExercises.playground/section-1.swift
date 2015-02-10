278532e32d5a86ed583974a8a20c496ab38644c8
Craig86322 <random.me@me.com>
2015-02-10 14:37:22 -0800

    Initial commit

---
+++ ClosureExercises.playground/section-1.swift
@@ -0,0 +1,128 @@
+// Closure Exercises.
+
+import UIKit
+
+// Three ways to create an empty Int array
+/*
+var array1 = Array<Int>()
+var array2 = [Int]()
+var array3: [Int] = []
+*/
+// Map *******
+/*
+println("map()")
+
+// Convert an array of temps in F to C
+// C = (F - 32) * ( 5 / 9)
+// F = C * ( 9 / 5 ) + 32
+
+var tempF = [-10.5, 32.0, 60.9, 80.3, 100.9, 115]
+
+// Pass the temperature conversion forumla to map() via a named function
+func cToF (t:Double) -> Double {
+    return ( t - 32 ) * (5 / 9 )
+}
+var tempC = tempF.map(cToF)
+println("1: \(tempC)")
+
+// Pass the forumla to map() via a closure
+println("2: \(tempF.map({($0 - 32) * (5 / 9)}))")
+
+// Create a string with 'C' appended to the temp and lose most of the decimals
+var tempC2 = tempC.map({"\(round($0))C"})
+println("3: \(tempC2)")
+
+//  Tricky point to note: Set the temperature array to all integers
+var tempFInt = [-10, 32, 60, 80, 100, 115]
+var tempC3 = tempFInt.map({($0 - 32) * (5 / 9)})
+// Prints '0' for each temp because the compiler infers integer math so 5/9 is truncated to 0
+println("4: \(tempC3)")
+*/
+// Filter *********
+/*
+println("filter()")
+
+// Use filter() to create arrays of even and odd numbers
+var numbers:[Int] = []
+for var i=0; i<=10; i++ {
+    numbers.append(i)
+}
+
+// pass a funtion to filter on odd numbers
+func oddFunc (x:Int) -> Bool {
+    return x % 2 == 1
+}
+println("\(numbers.filter(oddFunc))")
+
+// filter using closures
+let evens = numbers.filter({($0 % 2) == 0})
+let odds = numbers.filter({($0 % 2) == 1})
+println(evens)
+println(odds)
+*/
+// Reduce **************
+/*
+println("reduce()")
+
+// Init an array with digits from 0...10
+var numbers2:[Int] = []
+for var i=0; i<=10; i++ {
+    numbers2.append(i)
+}
+
+// pass a function to add all the numbers
+func addEmUp(x:Int, y:Int)->Int {
+    return x+y
+}
+println("\(numbers2.reduce(0,addEmUp))")
+
+// Add them up using a closure
+println("\(numbers2.reduce(0,{$0 + $1}))")
+
+// A compact version of the same reduce operation
+println("\(numbers2.reduce(0,+))")
+
+// Add them up starting at 100
+println("\(numbers2.reduce(100,+))")
+*/
+
+// Sort ********
+
+println("sort()")
+
+var nums = [5, 0, 9, 1, 3, 0, 8, 4]
+
+// the sort method sorts its own array
+
+// verbose version, using variable names and a return statement
+nums.sort({ x, y in return x > y })
+println(nums)
+
+// more compactly, default variables and implied return
+nums.sort({$0 < $1})
+println("\(nums)")
+
+// even more compactly, only the operator, everything else implied
+nums.sort(>)
+println(nums)
+
+// Note that nums.sort() sorts itself, but doesn't return a new array
+var newsort = nums.sort(<)
+// instead use...
+var newsort2 = nums
+// then
+newsort2.sort(>)
+println(newsort2)
+
+// Since no value is returned that's also why it doesn't work like you might expect in println()
+println("\(nums.sort(>)), \(nums)") // first arg returns (), second returns the value created by the first arg
+
+
+
+
+
+
+
+
+
+
